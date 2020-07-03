805378235d4a573216eec4afc505923fd71253e4
EdgarMung <lamborguini720@gmail.com>
2020-07-03 11:30:54 -0500

    Creacion del boton para generar AFN desde EXPRESIONES, Tambien actualizaciones en las funciones para mandar a llamar las ventanas

--- ventanaPrincipal.py
+++ ventanaPrincipal.py
@@ -1,22 +1,28 @@
 import tkinter as tk
-import ventanaGeneradorAFN, ventanaCalculadora, VentanaVerificadorGramaticas
+import ventanaGeneradorAFN, ventanaAFN_Expresion,ventanaCalculadora, VentanaVerificadorGramaticas
 
-#Funcion Crear VEntana Calculadora
+#Funcion Crear Ventana Verificador
 def abrirVerificador():
     ventanaSecundaria = tk.Toplevel(ventana)
-    verificador = VentanaVerificadorGramaticas.Crear_Ventana(master=ventanaSecundaria)
+    verificador = VentanaVerificadorGramaticas.ventanaVerificadorGramaticas(master=ventanaSecundaria)
     verificador.widges()
 
-#Funcion Crear VEntana Calculadora
+#Funcion Crear Ventana Calculadora
 def abrirCalculadora():
     ventanaSecundaria = tk.Toplevel(ventana)
-    Calculadora = ventanaCalculadora.Crear_VentanaCalculadora(master=ventanaSecundaria)
+    Calculadora = ventanaCalculadora.ventanaCalculadora(master=ventanaSecundaria)
     Calculadora.widges()
+
+#Funcion Crear Ventana AFN_Expresion
+def abrirGenerador_Expresion():
+    ventanaSecundaria = tk.Toplevel(ventana)
+    Generador = ventanaAFN_Expresion.ventanaAFN_Expresion(master=ventanaSecundaria)
+    Generador.widges()
 
 #Funcion Crear Ventana Generador AFN  
 def abrirMenuGeneradorAFN():
     ventanaSecundaria = tk.Toplevel(ventana)
-    Menu = ventanaGeneradorAFN.Crear_Ventana(master=ventanaSecundaria)
+    Menu = ventanaGeneradorAFN.ventanaGeneradorAFN(master=ventanaSecundaria)
     Menu.create_widgets()
 
 #Diseño de la pagina principal de todo el sistema ----------------------------------------------------------------
@@ -30,9 +36,10 @@ parteTitulo.pack()
 tk.Label(parteTitulo,text="Menu Principal",padx = 15, pady = 20, font=("Cantarell Extra Bold",20),bg = "floral white",fg = "black").pack()
 parteBotones = tk.Frame(ventana,bg = "blue")
 parteBotones.pack()
-tk.Button(parteBotones,text="Generador AFNś",font =("TSCu_Paranar",15),fg = "black",bg = "white",height = 2,width = 15 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirMenuGeneradorAFN).grid(column = 0, row = 0,pady = 10,padx = 5)
-tk.Button(parteBotones,text="Calculadora",font =("TSCu_Paranar",15),fg = "black" ,bg = "white",height = 2,width = 15 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirCalculadora).grid(column = 1, row = 0,pady = 10,padx = 5)
-tk.Button(parteBotones,text="Analizadores Sintacticos",font =("TSCu_Paranar",15),fg = "black" ,bg = "white",height = 2,width = 30 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirVerificador).grid(columnspan = 2, column = 0, row = 1,pady = 10,padx = 5)
+tk.Button(parteBotones,text="Generador AFN",font =("TSCu_Paranar",15),fg = "black",bg = "white",height = 2,width = 15 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirMenuGeneradorAFN).grid(column = 0, row = 0,pady = 10,padx = 5)
+tk.Button(parteBotones,text="Generador AFN_Expresion",font =("TSCu_Paranar",15),fg = "black",bg = "white",height = 2,width = 25 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirGenerador_Expresion).grid(column = 1, row = 0,pady = 10,padx = 5)
+tk.Button(parteBotones,text="Calculadora",font =("TSCu_Paranar",15),fg = "black" ,bg = "white",height = 2,width = 15 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirCalculadora).grid(column = 0, row = 1,pady = 10,padx = 5)
+tk.Button(parteBotones,text="Analizadores Sintacticos",font =("TSCu_Paranar",15),fg = "black" ,bg = "white",height = 2,width = 25 ,relief = "solid",activebackground = "cornflower blue",activeforeground = "white" ,bd = 3, command = abrirVerificador).grid(column = 1, row = 1,pady = 10,padx = 5)
 
 
 ventana.mainloop()
